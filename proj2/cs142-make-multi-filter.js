c46a300b64542ed50d7d186b721c69660d2a1729
Evan Shan <binrui.shan@gmail.com>
2022-11-29 22:06:02 -0800

    commit

---
+++ proj2/cs142-make-multi-filter.js
@@ -0,0 +1,17 @@
+// function is an object: it can have methods
+function cs142MakeMultiFilter(originalArray){
+
+    let currentArray = originalArray;
+
+    function arrayFilterer(filterCriteria, callback){
+        if (typeof filterCriteria !== 'function'){
+            return currentArray;
+        }
+        currentArray = currentArray.filter(filterCriteria);
+        if (typeof callback === "function"){
+            callback.call(originalArray, currentArray);
+        }
+        return arrayFilterer;
+    }
+    return arrayFilterer;
+}
